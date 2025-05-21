7b0b744f9ff32829b7f4e85fad56abdcbf16e989
Ahmad Mostafa <124518625+ahmadalasiri@users.noreply.github.com>
2025-05-21 22:42:38 +0300

    feat: create User entity with properties and relationships

---
+++ backend/src/users/user.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { Goal } from '../goals/goal.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Goal, (goal) => goal.owner)
+  goals: Goal[];
+}
