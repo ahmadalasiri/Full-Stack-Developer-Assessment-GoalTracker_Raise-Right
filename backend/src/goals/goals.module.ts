7f2c50fdf414b524caf273794f4e0745f43d0f61
Ahmad Mostafa <124518625+ahmadalasiri@users.noreply.github.com>
2025-05-22 18:14:36 +0300

    feat: update GoalsModule to include TypeOrmModule and AuthModule imports

--- backend/src/goals/goals.module.ts
+++ backend/src/goals/goals.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GoalsController } from './goals.controller';
 import { GoalsService } from './goals.service';
+import { Goal } from './goal.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Goal]), AuthModule],
   controllers: [GoalsController],
-  providers: [GoalsService]
+  providers: [GoalsService],
+  exports: [GoalsService],
 })
 export class GoalsModule {}
